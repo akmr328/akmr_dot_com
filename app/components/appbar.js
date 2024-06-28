7fe2062553ddee5b3c871647b97844b13681a2f9
Annie <annie@macbook-air-3.myfiosgateway.com>
2024-06-28 14:14:31 -0400

    brings appbar forward

--- app/components/appbar.js
+++ app/components/appbar.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const AppBar = () => {
     return (
-        <div className="fixed top-0 left-0 right-0 flex justify-between items-center bg-gray-200 bg-opacity-20 px-4 py-2 border-white border-2">
+        <div className="fixed top-0 left-0 z-50 right-0 flex justify-between items-center bg-gray-200 bg-opacity-20 px-4 py-2 border-white border-2">
            <h2 className="playfair-display text-4xl text-white">AKMR</h2>
             <div className="flex">
                 <Link href="/" className="mx-2 text-white text-lg font-serif">home</Link>
